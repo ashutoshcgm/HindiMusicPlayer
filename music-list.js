e68f5ae34db6e4eb6a37b9e75c5b6eaea02b69e3
ashutoshcgm <ashutoshexlcgm@gmail.com>
2022-05-10 19:01:47 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,66 @@
+let allMusic = [
+    {
+        name: "Bhul-Bhulaiya Title Track",
+        artist: "music-1",
+        img: "Music-1",
+        src: "Music-1"
+    },
+    {
+        name: "Dua Karo",
+        artist: "music-2",
+        img: "Music-2",
+        src: "Music-2"
+    },
+    {
+        name: "Jabra Fan",
+        artist: "music-3",
+        img: "Music-3",
+        src: "Music-3"
+    },
+    {
+        name: "Jee Karda",
+        artist: "music-4",
+        img: "Music-4",
+        src: "Music-4"
+    },
+    {
+        name: "Johnny Johnny - Entertainment",
+        artist: "music-5",
+        img: "Music-5",
+        src: "Music-5"
+    },
+    {
+        name: "Jugnu",
+        artist: "music-6",
+        img: "Music-6",
+        src: "Music-6"
+    }
+    ,
+    {
+        name: "Kabhi Tumhe  ",
+        artist: "music-7",
+        img: "Music-7",
+        src: "Music-7"
+    }
+    ,
+    {
+        name: "Mere Yaraa",
+        artist: "music-8",
+        img: "Music-8",
+        src: "Music-8"
+    }
+    ,
+    {
+        name: "Nainowale Ne",
+        artist: "music-9",
+        img: "Music-9",
+        src: "Music-9"
+    }
+    ,
+    {
+        name: "Najaa",
+        artist: "music-10",
+        img: "Music-10",
+        src: "Music-10"
+    }
+];
